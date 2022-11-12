565efafb5d437c0e3e9e9f4daceaa65fd0e2d4af
licheng1013 <46236080+licheng1013@users.noreply.github.com>
2022-11-12 07:20:50 +0000

    Update gh-pages to output generated at 31f5baf

---
+++ assets/gorm.html.27457234.js
@@ -0,0 +1,36 @@
+import{_ as n,o as s,c as a,b as t}from"./app.51ed7218.js";const e={},o=t(`<h1 id="gorm" tabindex="-1"><a class="header-anchor" href="#gorm" aria-hidden="true">#</a> Gorm</h1><h2 id="\u4ECB\u7ECD" tabindex="-1"><a class="header-anchor" href="#\u4ECB\u7ECD" aria-hidden="true">#</a> \u4ECB\u7ECD</h2><ul><li>2022/9/23</li></ul><h2 id="\u5B89\u88C5" tabindex="-1"><a class="header-anchor" href="#\u5B89\u88C5" aria-hidden="true">#</a> \u5B89\u88C5</h2><ul><li>go get -u gorm.io/gorm</li><li>go get -u github.com/go-sql-driver/mysql</li><li>go get -u gorm.io/driver/mysql v1.3.5</li></ul><h2 id="\u4F7F\u7528" tabindex="-1"><a class="header-anchor" href="#\u4F7F\u7528" aria-hidden="true">#</a> \u4F7F\u7528</h2><ul><li>\u4E0B\u9762\u4EE3\u7801\u590D\u5236\u5B8C\u8FD0\u884C\u8FD9\u884C\u4EE3\u7801\u540E\u4F1A\u81EA\u52A8\u641C\u7D22\u5305: go mod tidy</li></ul><div class="language-go ext-go line-numbers-mode"><pre class="language-go"><code><span class="token keyword">package</span> config
+
+<span class="token keyword">import</span> <span class="token punctuation">(</span>
+	<span class="token string">&quot;gin-gin/common&quot;</span>
+	<span class="token string">&quot;gorm.io/driver/mysql&quot;</span>
+	<span class="token string">&quot;gorm.io/gorm&quot;</span>
+	<span class="token string">&quot;gorm.io/gorm/logger&quot;</span>
+	<span class="token string">&quot;gorm.io/gorm/schema&quot;</span>
+	<span class="token string">&quot;log&quot;</span>
+<span class="token punctuation">)</span>
+
+<span class="token comment">// \u521D\u59CB\u5316mysql\u94FE\u63A5</span>
+<span class="token keyword">func</span> <span class="token function">init</span><span class="token punctuation">(</span><span class="token punctuation">)</span> <span class="token punctuation">{</span>
+	<span class="token function">mysqlInit</span><span class="token punctuation">(</span><span class="token punctuation">)</span>
+<span class="token punctuation">}</span>
+
+<span class="token keyword">func</span> <span class="token function">mysqlInit</span><span class="token punctuation">(</span><span class="token punctuation">)</span> <span class="token punctuation">{</span>
+	<span class="token comment">// \u65E5\u5FD7\u6253\u5370</span>
+	newLogger <span class="token operator">:=</span> logger<span class="token punctuation">.</span>Default
+
+	log<span class="token punctuation">.</span><span class="token function">Println</span><span class="token punctuation">(</span><span class="token string">&quot;Mysql:\u521D\u59CB\u5316\uFF01&quot;</span><span class="token punctuation">)</span>
+	dsn <span class="token operator">:=</span> <span class="token string">&quot;root:root@tcp(127.0.0.1:3306)/t_demo?charset=utf8mb4&amp;parseTime=True&amp;loc=Local&quot;</span>
+	v<span class="token punctuation">,</span> err <span class="token operator">:=</span> gorm<span class="token punctuation">.</span><span class="token function">Open</span><span class="token punctuation">(</span>mysql<span class="token punctuation">.</span><span class="token function">Open</span><span class="token punctuation">(</span>dsn<span class="token punctuation">)</span><span class="token punctuation">,</span> <span class="token operator">&amp;</span>gorm<span class="token punctuation">.</span>Config<span class="token punctuation">{</span>
+		Logger<span class="token punctuation">:</span> newLogger<span class="token punctuation">,</span>
+		NamingStrategy<span class="token punctuation">:</span> schema<span class="token punctuation">.</span>NamingStrategy<span class="token punctuation">{</span>
+			TablePrefix<span class="token punctuation">:</span>   <span class="token string">&quot;t_&quot;</span><span class="token punctuation">,</span> <span class="token comment">// \u5B9A\u4E49\u8868\u524D\u7F00</span>
+			SingularTable<span class="token punctuation">:</span> <span class="token boolean">true</span><span class="token punctuation">,</span> <span class="token comment">// true\u4E0D\u5728\u8868\u540E\u9762+ s\uFF0C</span>
+		<span class="token punctuation">}</span><span class="token punctuation">,</span>
+	<span class="token punctuation">}</span><span class="token punctuation">)</span>
+	<span class="token keyword">if</span> err <span class="token operator">!=</span> <span class="token boolean">nil</span> <span class="token punctuation">{</span>
+		log<span class="token punctuation">.</span><span class="token function">Panic</span><span class="token punctuation">(</span>err<span class="token punctuation">)</span>
+	<span class="token punctuation">}</span>
+	common<span class="token punctuation">.</span>Db <span class="token operator">=</span> v
+<span class="token punctuation">}</span>
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,8),i=[o];function p(c,l){return s(),a("div",null,i)}const r=n(e,[["render",p],["__file","gorm.html.vue"]]);export{r as default};
